b8d647ae0138390cbd2919f0b8f2970f7c7c6540
santani423 <santani423@gmail.com>
2026-03-23 16:58:26 +0700

    oke

--- app/Controllers/base/Beranda.php
+++ app/Controllers/base/Beranda.php
@@ -49,8 +49,8 @@ class Beranda extends Controller
 		$data['title'] = 'Beranda';
 		//load view home
 		$data['view'] = 'base/beranda/home';
-		return view('base/beranda/layout', $data);
-		return view('base/beranda/layout/index');
+		// return view('base/beranda/layout', $data);
+		// return view('base/beranda/layout/index');
 
 	}
 
